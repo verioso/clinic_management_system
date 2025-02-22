d250be13f441b4e092c5b5354f3be7a9b72bce5a
verioso <mjverioso@gmail.com>
2025-02-22 16:01:13 +0800

    Clinic system GUI

---
+++ src/Config/config.java
@@ -0,0 +1,42 @@
+package Config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class config {
+    private Connection connect;
+
+       // constructor to connect to our database
+        public config(){
+            try{
+                connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/clinic_management", "root", "");
+            }catch(SQLException ex){
+                    System.out.println("Can't connect to database: "+ex.getMessage());
+            }
+        }
+        //Function to save data
+        public int insertData(String sql){
+            int result;
+            try{
+                PreparedStatement pst = connect.prepareStatement(sql);
+                pst.executeUpdate();
+                System.out.println("Inserted Successfully!");
+                pst.close();
+                result =1;
+            }catch(SQLException ex){
+                System.out.println("Connection Error: "+ex);
+                result =0;
+            }
+            return result;
+        }
+        //Function to retrieve data
+        public ResultSet getData(String sql) throws SQLException{
+            Statement stmt = connect.createStatement();
+            ResultSet rst = stmt.executeQuery(sql);
+            return rst;
+        }
+}
